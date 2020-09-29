41957e8f08db0a3b17daee3267016da6e899772b
Paul Costa <admin@paulcosta.at>
2020-09-29 16:34:41 +0200

    -

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
 
-  GOOGLE_API_KEY:'XXX',
-  DARKSKY_API_KEY:'XXX',
+  GOOGLE_API_KEY:'AIzaSyBeVDt6M48IiC8Xdm-pn-CouhJ5GzOuyNk',
+  DARKSKY_API_KEY:'52c6984aa80b6b1fc50d3dd340934b5b',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
 
-  GOOGLE_API_KEY:'XXX',
-  DARKSKY_API_KEY:'XXX',
+  GOOGLE_API_KEY:'AIzaSyBeVDt6M48IiC8Xdm-pn-CouhJ5GzOuyNk',
+  DARKSKY_API_KEY:'52c6984aa80b6b1fc50d3dd340934b5b',
 };
 
 /*
